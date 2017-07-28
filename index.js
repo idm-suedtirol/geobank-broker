4b134647a7369c3eab650cd03e670cb7d7a9be3f
rthoeni <rudi_80@gmx.net>
2017-07-28 11:56:41 +0200

    expanded database.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/ls';
+//var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/ls';
 var http = require('http');
-var pg_client= new pg.Client(connectionString);
-pg_client.connect();
+var db = require('./models/database');
 var url = require('url');
+
 http.createServer(function(req,res){
 	var route = url.parse(req.url).pathname.replace("/","");
 	res.writeHead(200, {'Content-Type': 'application/json'});
@@ -12,8 +12,14 @@ http.createServer(function(req,res){
 		var services = getServices();
 		var json = JSON.stringify(services);
 		res.end(json);
-	}else {
-		res.end(JSON.stringify(getDetail(route)));
+	}
+	else if(route != 'favicon.ico') {
+
+		var myresult = getDetail(route);
+		console.log(myresult);
+
+		//res.end(JSON.stringify(getDetail(route)));
+		res.end(myresult);
 	}
 }).listen(8090);
 
@@ -21,11 +27,14 @@ function getServices(){
 	return ["meteo","accomodation","event","environment"]
 }
 function getDetail(service){
-	if (service === "meteo"){
-		return {
-			id:125,
-			formats:["xml","json","jsonp"],
-			url:"http://patrick.ohnewein.nit"
-		}
-	}
+
+	return db.selectFromGeobankTable(service);
+
+	// if (service === "meteo"){
+	// 	return {
+	// 		id:125,
+	// 		formats:["xml","json","jsonp"],
+	// 		url:"http://patrick.ohnewein.nit"
+	// 	}
+	// }
 }
--- models/database.js
+++ models/database.js
@@ -9,29 +9,96 @@ const pool = new Pool({
   port: 5432,
 });
 
-pool.query(
-    'CREATE TABLE geobank(id SERIAL PRIMARY KEY, data JSONB)' , (err, res) => {
-    console.log(err, res);
-    pool.end();
+//TODO
+//createGeobankTable();
+//dropGeobankTable();
+//fillGeobankTable();
+//updateGeobankTable();
+//selectFromGeobankTable('meteo');
+
+
+//Create Object Array for Table
+function getGeobankObjectArray(){
+  var mytypes = [];
+  mytypes.push({ identifier:'meteo', data: '{ "name":"meteo", "url":"http://service.suedtirol.info/api/Weather" }' })
+  mytypes.push({ identifier:'accommodation', data: '{ "name":"accommodation", "url":"http://service.suedtirol.info/api/Accommodation" }' })
+  mytypes.push({ identifier:'event', data: '{ "name":"event", "url":"http://service.suedtirol.info/api/Event"}' })
+  mytypes.push({ identifier:'gastronomy', data: '{ "name":"gastronomy", "url":"http://service.suedtirol.info/api/Gastronomy"}' })
+  mytypes.push({ identifier:'activity', data: '{ "name":"activity", "url":"http://service.suedtirol.info/api/Activity"}' })
+  mytypes.push({ identifier:'poi', data: '{ "name":"poi", "url":"http://service.suedtirol.info/api/Poi"}' })
+
+  return mytypes;
+};
+
+//CREATE Table
+function createGeobankTable(){
+  pool.query(
+      'CREATE TABLE geobank(id SERIAL PRIMARY KEY, identifier VARCHAR(20) UNIQUE, data JSONB)' , (err, res) => {
+      console.log(err, res);
+      pool.end();
+    });
+};
+
+//DROP Table
+function dropGeobankTable(){
+  pool.query(
+      'DROP Table geobank' , (err, res) => {
+      console.log(err, res);
+      pool.end();
+    });
+};
+
+//INSERT INTO TABLE
+function fillGeobankTable(){
+
+  var arr = getGeobankObjectArray();
+
+  arr.forEach(function(value){
+    console.log("Inserting:" + value.identifier);
+    pool.query(
+      //'INSERT INTO geobank (identifier,data) VALUES(\'' + value.identifier + '\',\'' + value.data + '\')' , (err, res) => {
+      'INSERT INTO geobank (identifier,data) VALUES($1,$2)', [ value.identifier, value.data] , (err, res) => {
+        console.log(err, res);
+    });
+  });
+  pool.end();
+};
+
+//UPDATE TABLE
+function updateGeobankTable(){
+
+  var arr = getGeobankObjectArray();
+
+  arr.forEach(function(value){
+    console.log("Updating:" + value.identifier);
+    pool.query(
+      'UPDATE geobank SET data = $1 WHERE identifier = $2', [ value.data, value.identifier ] , (err, res) => {
+        console.log(err, res);
+    });
+  });
+  pool.end();
+};
+
+//SELECT LIST FROM TABLE
+
+//SELECT FROM TABLE
+exports.selectFromGeobankTable = function(identifier){
+  console.log("Selecting:" + identifier);
+
+  pool.query(
+    'SELECT data FROM geobank WHERE identifier = $1', [ identifier ] , (err, res) => {
+
+      if (err) {
+          return err;
+        }
+      if(res)
+      {
+        console.log(res.rows[0].data);
+
+        return res.rows[0].data;
+      }
+      //pool.end();
   });
 
-// pool.connect(function(err,client,done) {
-//   client.query(
-//     'CREATE TABLE geobank(id SERIAL PRIMARY KEY, data JSONB)' , (err, res) => {
-//     console.log("finito");
-//     done();
-//   });
-// });
-//
-// pool.end();
 
-
- //client = new pg.Client(connectionString);
-//client.connect();
-//var query = client.query(
-//  'CREATE TABLE geobank(id SERIAL PRIMARY KEY, data JSONB)');
-//client.end();
-//query.on('end', () => { client.end(); });
-//query.on('end', function() {
-    // client.end();
-// });
+};
