e14690a13084975f57b6b71a4b7ce85e4e47e093
rthoeni <rudi_80@gmx.net>
2017-08-21 09:41:56 +0200

    structure changed

--- models/database.js
+++ models/database.js
@@ -2,10 +2,10 @@ const { Pool, Client } = require('pg');
 //const connectionString = process.env.DATABASE_URL || 'postgres://postgres:testhallo@localhost:5432/geobank';
 
 const pool = new Pool({
-  user: 'patrick',
+  user: 'postgres',
   host: 'localhost',
   database: 'geobank',
-  password: '',
+  password: 'testhallo',
   port: 5432,
 });
 
@@ -20,36 +20,51 @@ const pool = new Pool({
 //Create Object Array for Table
 function getGeobankObjectArray(){
   var mytypes = [];
-  mytypes.push({ identifier:'meteo', data: '['+
-    '{ "origin":"Siag", "url":"http://service.suedtirol.info/api/Weather", "doc":"http://service.suedtirol.info/help" },'+
-    '{ "origin":"province BZ", "url":"http://ipchannels.integreen-life.bz.it/MeteoFrontEnd", "doc":"http://ipchannels.integreen-life.bz.it/MeteoFrontEnd","dataFormats":"json"}'+
-  ']'
-  })
-  mytypes.push({ identifier:'accommodation', data: '[ { "origin":"LTS", "url":"http://service.suedtirol.info/api/Accommodation", "doc":"http://service.suedtirol.info/help" } ]' })
-  mytypes.push({ identifier:'event', data: '[ { "origin":"LTS", "url":"http://service.suedtirol.info/api/Eventi", "doc":"http://service.suedtirol.info/help" } ]' })
-  mytypes.push({ identifier:'gastronomy', data: '[ { "origin":"LTS", "url":"http://service.suedtirol.info/api/Gastronomy", "doc":"http://service.suedtirol.info/help" } ]' })
-  mytypes.push({ identifier:'activity', data: '[ { "origin":"LTS", "url":"http://service.suedtirol.info/api/Activity" , "doc":"http://service.suedtirol.info/help"} ]' })
-  mytypes.push({ identifier:'poi', data: '[ { "origin":"LTS", "url":"http://service.suedtirol.info/api/Poi" , "doc":"http://service.suedtirol.info/help"} ]' })
-  mytypes.push({ identifier:'environment', data: '[ { "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd"} ]' })
-  mytypes.push({ identifier:'parking', data: '[ { "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/parkingFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/parkingFrontEnd"} ]' })
-  mytypes.push({ identifier:'environment', data: '[ { "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd"} ]' })
-  mytypes.push({ identifier:'traffic', data: '[ '+
-    '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/BluetoothFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/BluetoothFrontEnd"},'+
-    '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/LinkFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/LinkFrontEnd"},'+
-    '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/StreetFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/StreetFrontEnd"},'+
-    '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/TrafficFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/TrafficFrontEnd"}'+
-  ']' })
+  // mytypes.push({ identifier:'meteogb', data: '{ "origin":"Siag", "url":"http://service.suedtirol.info/api/Weather", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":["meteo"] }'});
+  // mytypes.push({ identifier:'meteointegreen', data: '{ "origin":"province BZ", "url":"http://ipchannels.integreen-life.bz.it/MeteoFrontEnd", "doc":"http://ipchannels.integreen-life.bz.it/MeteoFrontEnd","dataFormats":"json", "tag":["meteo"] }'});
+  // mytypes.push({ identifier:'accommodation', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Accommodation", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":["accommodation","tourism"] }' });
+  // mytypes.push({ identifier:'event', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Eventi", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":["events","tourism"] }' });
+  // mytypes.push({ identifier:'gastronomy', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Gastronomy", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":["gastronomy"] }' });
+  // mytypes.push({ identifier:'activity', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Activity" , "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":["activity"] }' });
+  // mytypes.push({ identifier:'poi', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Poi" , "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":["poi"] }' });
+  // mytypes.push({ identifier:'parking', data: '{ "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/parkingFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/parkingFrontEnd","dataFormats":"json", "tag":["integreen","parking"] }' });
+  // mytypes.push({ identifier:'environment', data: '{ "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd","dataFormats":"json", "tag":["integreen","parking"] }' });
+  // mytypes.push({ identifier:'traffic_btfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/BluetoothFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/BluetoothFrontEnd","dataFormats":"json", "tag":["integreen","traffic"] }' });
+  // mytypes.push({ identifier:'traffic_linkfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/LinkFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/LinkFrontEnd","dataFormats":"json", "tag":["integreen","traffic"] }' });
+  // mytypes.push({ identifier:'traffic_streetfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/StreetFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/StreetFrontEnd","dataFormats":"json", "tag":["integreen","traffic"] }' });
+  // mytypes.push({ identifier:'traffic_trafficfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/TrafficFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/TrafficFrontEnd","dataFormats":"json", "tag":["integreen","traffic"] }' });
+
+
+//brings a net
+  mytypes.push({ identifier:'meteogb', data: '{ "origin":"Siag", "url":"http://service.suedtirol.info/api/Weather", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":[{ "tagname":"meteo" }] }'});
+  mytypes.push({ identifier:'meteointegreen', data: '{ "origin":"province BZ", "url":"http://ipchannels.integreen-life.bz.it/MeteoFrontEnd", "doc":"http://ipchannels.integreen-life.bz.it/MeteoFrontEnd","dataFormats":"json", "tag":[{ "tagname":"meteo" }] }'});
+  mytypes.push({ identifier:'accommodation', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Accommodation", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":[{ "tagname":"accommodation" },{ "tagname":"tourism" }] }' });
+  mytypes.push({ identifier:'event', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Eventi", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":[{ "tagname":"events" },{ "tagname":"tourism" }] }' });
+  mytypes.push({ identifier:'gastronomy', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Gastronomy", "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":[{ "tagname":"gastronomy" }] }' });
+  mytypes.push({ identifier:'activity', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Activity" , "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":[{ "tagname":"activity" }] }' });
+  mytypes.push({ identifier:'poi', data: '{ "origin":"LTS", "url":"http://service.suedtirol.info/api/Poi" , "doc":"http://service.suedtirol.info/help","dataFormats":"json", "tag":[{ "tagname":"poi" }] }' });
+  mytypes.push({ identifier:'parking', data: '{ "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/parkingFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/parkingFrontEnd","dataFormats":"json", "tag":[{ "tagname":"integreen" },{ "tagname":"parking" }] }' });
+  mytypes.push({ identifier:'environment', data: '{ "origin":"Province BZ", "url":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/EnvironmentFrontEnd","dataFormats":"json", "tag":[{ "tagname":"integreen" },{ "tagname":"parking" }] }' });
+  mytypes.push({ identifier:'traffic_btfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/BluetoothFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/BluetoothFrontEnd","dataFormats":"json", "tag":[{ "tagname":"integreen" },{ "tagname":"traffic" }] }' });
+  mytypes.push({ identifier:'traffic_linkfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/LinkFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/LinkFrontEnd","dataFormats":"json", "tag":[{ "tagname":"integreen" },{ "tagname":"traffic" }] }' });
+  mytypes.push({ identifier:'traffic_streetfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/StreetFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/StreetFrontEnd","dataFormats":"json", "tag":[{ "tagname":"integreen" },{ "tagname":"traffic" }] }' });
+  mytypes.push({ identifier:'traffic_trafficfrontend', data: '{ "origin":"IDM-Suedtirol", "url":"http://ipchannels.integreen-life.bz.it/TrafficFrontEnd" , "doc":"http://ipchannels.integreen-life.bz.it/TrafficFrontEnd","dataFormats":"json", "tag":[{ "tagname":"integreen" },{ "tagname":"traffic" }] }' });
+
+
   return mytypes;
 };
+
 
 //CREATE Table
 function createGeobankTable(){
   pool.query(
-      'CREATE TABLE geobank(id SERIAL PRIMARY KEY, identifier VARCHAR(20) UNIQUE, data JSONB)' , (err, res) => {
+      'CREATE TABLE geobank(id SERIAL PRIMARY KEY, identifier VARCHAR(50) UNIQUE, data JSONB)' , (err, res) => {
       console.log(err, res);
       pool.end();
     });
 };
+
+
 
 //DROP Table
 function dropGeobankTable(){
@@ -73,7 +88,9 @@ function fillGeobankTable(){
         console.log(err, res);
     });
   });
-  pool.end();
+
+  //tuater vorher schun zua
+  //pool.end();
 };
 
 //UPDATE TABLE
